cf688de7845f4c1976008c26358d7fba3c604843
Gaurav Arora <gauravarora@192.168.1.10>
2024-09-03 16:25:31 +0530

    commit -connect API resources to specific URL paths

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from flask_restful import Api
+from .resources import UserResource, TaskResource
+
+def initialize_routes(api: Api):
+    # route for managing a single user
+    api.add_resource(UserResource, '/users/<int:user_id>', endpoint='user')
+
+    # route for managing a single task
+    api.add_resource(TaskResource, '/tasks/<int:task_id>', endpoint='task')
+
+    # route for managing all tasks (create new tasks)
+    api.add_resource(TaskResource, '/tasks', endpoint='tasks')
