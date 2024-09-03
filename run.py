fdee9927d2816a5527dbcf4904158d3e257b303f
Gaurav Arora <gauravarora@192.168.1.10>
2024-09-03 17:10:50 +0530

    bug fix

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import create_app, db
+from app import create_app  # db
 from app.routes import initialize_routes
 from flask_restful import Api
 
@@ -7,11 +7,13 @@ app = create_app()
 
 # initialize the API with Flask app
 api = Api(app)
+
 initialize_routes(api)
 
 # create the database tables
-with app.app_context():
+'''with app.app_context():
     db.create_all()
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
